2b17ea83adaa999f1f9b10ab8e3eb766bd282ceb
harris012 <harissaeed@protonmail.com>
2022-04-24 23:35:27 +0200

    Why not getting the data of whole country?

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 region = 193
-URL = 'https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?where=OBJECTID={} &outFields=OBJECTID, GEN, BEZ, BL, cases, deaths, cases_per_population, cases7_per_100k, cases7_lk, death7_lk, cases7_bl_per_100k, cases7_bl, death7_bl,last_update&outSR=4326&f=json' .format(region)
+URL = 'https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?where=1%3D1&outFields=GEN,BEZ,BL,cases7_bl_per_100k,cases7_bl,death7_bl,cases,deaths,cases_per_100k,cases_per_population,BL_ID,last_update,cases7_lk,death7_lk&outSR=4326&f=json'
 result = requests.get(URL)
 
 output = json.loads(result.text)
