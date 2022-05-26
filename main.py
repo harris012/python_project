e887a00e8a014dc7e3cc0c2e7bdecb9397393385
harris012 <harissaeed@protonmail.com>
2022-05-27 01:38:12 +0200

    query api and save data to influxdb

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 import requests
 import json
+from influxdb import InfluxDBClient
 
-region = 193
+client = InfluxDBClient(host="100.68.129.77", port=8086)
+client.switch_database("covid")
+
 URL = 'https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?where=1%3D1&outFields=GEN,BEZ,BL,cases7_bl_per_100k,cases7_bl,death7_bl,cases,deaths,cases_per_100k,cases_per_population,last_update,cases7_lk,death7_lk&outSR=4326&f=json'
 result = requests.get(URL)
 
 output = json.loads(result.text)
 refined = output['features']
 data = [{k: v for k, v in d.items() if k == 'attributes'} for d in refined]
+for d in data:
+	json_body = [{
+		"measurement": "GermanyData",
+		"tags": {"State": d['attributes']["BL"]},
+		"fields":{
+			"city": d['attributes']["GEN"],
+			"cases":d['attributes']["cases"],
+			"cases7_bl":d['attributes']["cases7_bl"],
+			"cases_per_100k":d['attributes']["cases_per_100k"],
+			"cases7_bl_per_100k":d['attributes']["cases7_bl_per_100k"],
+			"cases7_lk":d['attributes']["cases7_lk"],
+			"cases_per_population":d['attributes']["cases_per_population"],
+			"deaths":d['attributes']["deaths"],
+			"death7_bl":d['attributes']["death7_bl"],
+			"death7_lk":d['attributes']["death7_lk"]
+		}}]
+	client.write_points(json_body)
 
-print(data)
-with open('data.json', 'w', encoding='utf-8') as f:
-    json.dump(data, f, ensure_ascii=False, indent=2, sort_keys=True)
-
+print('done')
